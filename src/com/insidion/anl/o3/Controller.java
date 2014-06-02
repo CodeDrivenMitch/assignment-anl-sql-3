33af8da6f9146fe9cea22c223b1f04d6c3ec9349
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2014-06-02 12:30:12 +0200

    Logging added

--- src/com/insidion/anl/o3/Controller.java
+++ src/com/insidion/anl/o3/Controller.java
@@ -1,5 +1,7 @@
 package com.insidion.anl.o3;
 
+import org.apache.log4j.Logger;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,10 +10,12 @@ import java.sql.SQLException;
  * Created by mitchell on 6/2/2014.
  */
 public class Controller {
+    static Logger log = Logger.getLogger(Controller.class.getName());
     Connection connection = null;
 
     public Controller() throws SQLException {
         connection = DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/Opdracht3", "anl", "anl");
+        log.info("Database conection initialized");
     }
 }
